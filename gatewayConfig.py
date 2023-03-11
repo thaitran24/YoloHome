5486ba86c100b94fa744d7c793f288009ad6a3f5
ptmsk <th.n.h.2.40.31@gmail.com>
2023-03-11 23:38:42 +0700

    add gateway class utility

---
+++ gatewayConfig.py
@@ -0,0 +1,70 @@
+import sys
+from Adafruit_IO import MQTTClient, Client
+
+import random
+import time
+
+class gatewayConfig:
+    def __init__(self, aio_username, aio_key) -> None:
+        self.aio_username = aio_username
+        self.aio_key = aio_key
+
+        self.client = Client(self.aio_username, self.aio_key)
+        self.mqttclient = MQTTClient(self.aio_username, self.aio_key)
+
+        self.aio_feed_id = [f.key for f in self.client.feeds()]
+
+        self.__callback()
+
+    def __callback(self):
+        self.mqttclient.on_connect = self.__connected
+        self.mqttclient.on_disconnect = self.__disconnected
+        self.mqttclient.on_message = self.__message
+        self.mqttclient.on_subscribe = self.__subscribe
+        self.mqttclient.on_unsubscribe = self.__unsubscribe
+
+    def __connected(self, client):
+        print("Connected successfully...")
+        for feed in self.aio_feed_id:
+            client.subscribe(feed)
+
+    def __subscribe(self, client, userData, topic, granted_qos):
+        print("Subscribed to {0} with QOS level {1}".format(topic, granted_qos))
+
+    def __unsubscribe(self, client, userData, topic, pid):
+        print("Unsubscribed from {0} with PID {1}".format(topic, pid))
+
+    def __disconnected(self, client):
+        print("Disconnected...")
+        sys.exit(1)
+
+    def __message(self, client, feed_id, payload):
+        print("Feed {0} received new value: {1}".format(feed_id, payload))
+
+    def publishData(self):
+        data = []
+
+        value = random.randint(0, 100)
+        print('Update:', value)
+
+        data.append(value%5 * 25)           # Fan: 0, 25, 50, 75, 100
+        data.append(value%2)                # Servo: 0, 1
+        data.extend([value%2, (value+1)%2]) # led1, led2: 0, 1
+        data.append(value)                  # light sensor (0-100)
+        data.append(value)                  # humid sensor (0-100)
+        data.append(value)                  # temperature (0-100)
+
+        for id, value in zip(self.aio_feed_id, data):
+            self.mqttclient.publish(id, value)        
+
+    def run(self):
+        self.mqttclient.connect()
+        self.mqttclient.loop_background()
+
+        while True:
+            self.publishData()
+            
+            time.sleep(15)
+
+if __name__ == "__main__":
+    print("Gateway Configuration Utility")
--- main.py
+++ main.py
@@ -1,60 +1,9 @@
-import sys
-from Adafruit_IO import MQTTClient
-
-import random
-import time
+from gatewayConfig import gatewayConfig
 
 from api_key import *
 
 
-"""MQTT PROTOCOL"""
-def connected(client):
-    print("Connected successfully...")
-    for feed in AIO_FEED_ID:
-        client.subscribe(feed)
-    for feed in AIO_FEED_ID2:
-        client.subscribe(feed)
-
-
-
-def subscribe(client, userData, mid, granted_qos):
-    print("Subscribe successfully...")
-
-
-def disconnected(client):
-    print("Disconnected...")
-    sys.exit(1)
-
-# def processData()
-
-
-def message(client, feed_id, payload):
-    print("Get data: " + feed_id + ' ' + payload)
-
-
-
 if __name__ == '__main__':
+    gc = gatewayConfig(AIO_USERNAME, AIO_KEY)
 
-    """GATEWAY CONFIGURATION"""
-    client = MQTTClient(AIO_USERNAME, AIO_KEY)
-    client.on_connect = connected
-    client.on_disconnect = disconnected
-    client.on_message = message
-    client.on_subscribe = subscribe
-    client.connect()
-    client.loop_background()
-
-    while True:
-        value = random.randint(0, 100)
-        print('Update:', value)
-
-        client.publish('yolohome-full.lightsensor', value)
-        client.publish('yolohome-full.humidsensor', value)
-        client.publish('yolohome-full.tempsensor', value/2)
-        client.publish('yolohome-full.led1', value%2)
-        client.publish('yolohome-nosensor.fan', value%5 * 25)
-
-
-
-
-        time.sleep(15)
+    gc.run()
