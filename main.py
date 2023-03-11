0ab168c63ce52ff83dbbfd0765e7ea4b6f758603
ptmsk <th.n.h.2.40.31@gmail.com>
2023-03-11 22:38:14 +0700

    test gateway

--- main.py
+++ main.py
@@ -1,3 +1,60 @@
+import sys
+from Adafruit_IO import MQTTClient
 
-if __name__ == "__main__":
-    pass
+import random
+import time
+
+from api_key import *
+
+
+"""MQTT PROTOCOL"""
+def connected(client):
+    print("Connected successfully...")
+    for feed in AIO_FEED_ID:
+        client.subscribe(feed)
+    for feed in AIO_FEED_ID2:
+        client.subscribe(feed)
+
+
+
+def subscribe(client, userData, mid, granted_qos):
+    print("Subscribe successfully...")
+
+
+def disconnected(client):
+    print("Disconnected...")
+    sys.exit(1)
+
+# def processData()
+
+
+def message(client, feed_id, payload):
+    print("Get data: " + feed_id + ' ' + payload)
+
+
+
+if __name__ == '__main__':
+
+    """GATEWAY CONFIGURATION"""
+    client = MQTTClient(AIO_USERNAME, AIO_KEY)
+    client.on_connect = connected
+    client.on_disconnect = disconnected
+    client.on_message = message
+    client.on_subscribe = subscribe
+    client.connect()
+    client.loop_background()
+
+    while True:
+        value = random.randint(0, 100)
+        print('Update:', value)
+
+        client.publish('yolohome-full.lightsensor', value)
+        client.publish('yolohome-full.humidsensor', value)
+        client.publish('yolohome-full.tempsensor', value/2)
+        client.publish('yolohome-full.led1', value%2)
+        client.publish('yolohome-nosensor.fan', value%5 * 25)
+
+
+
+
+        time.sleep(15)
