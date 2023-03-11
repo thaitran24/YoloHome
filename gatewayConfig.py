231e2b68965c7c7aecd70d64a385a8bad451f2e1
ptmsk <th.n.h.2.40.31@gmail.com>
2023-03-12 00:00:33 +0700

    add method description

--- gatewayConfig.py
+++ gatewayConfig.py
@@ -12,11 +12,13 @@ class gatewayConfig:
         self.client = Client(self.aio_username, self.aio_key)
         self.mqttclient = MQTTClient(self.aio_username, self.aio_key)
 
+        # retrieve feed ids from Client
         self.aio_feed_id = [f.key for f in self.client.feeds()]
 
         self.__callback()
 
     def __callback(self):
+        """Connect the callback methods defined above to Adafruit IO"""
         self.mqttclient.on_connect = self.__connected
         self.mqttclient.on_disconnect = self.__disconnected
         self.mqttclient.on_message = self.__message
@@ -24,24 +26,32 @@ class gatewayConfig:
         self.mqttclient.on_unsubscribe = self.__unsubscribe
 
     def __connected(self, client):
+        # Connected function will be called when the client is connected to Adafruit IO.
         print("Connected successfully...")
         for feed in self.aio_feed_id:
             client.subscribe(feed)
 
     def __subscribe(self, client, userData, topic, granted_qos):
+        # This method is called when the client subscribes to a new feed.
         print("Subscribed to {0} with QOS level {1}".format(topic, granted_qos))
 
     def __unsubscribe(self, client, userData, topic, pid):
+        # This method is called when the client unsubscribes from a feed.
         print("Unsubscribed from {0} with PID {1}".format(topic, pid))
 
     def __disconnected(self, client):
+        # Disconnected function will be called when the client disconnects.        
         print("Disconnected...")
         sys.exit(1)
 
     def __message(self, client, feed_id, payload):
+        # Message function will be called when a subscribed feed has a new value.
+        # The feed_id parameter identifies the feed, and the payload parameter has
+        # the new value.
         print("Feed {0} received new value: {1}".format(feed_id, payload))
 
     def publishData(self):
+        # Collect data and publish it to Adafruit Server
         data = []
 
         value = random.randint(0, 100)
@@ -54,6 +64,7 @@ class gatewayConfig:
         data.append(value)                  # humid sensor (0-100)
         data.append(value)                  # temperature (0-100)
 
+        # Publish data with specific feed_id
         for id, value in zip(self.aio_feed_id, data):
             self.mqttclient.publish(id, value)        
 
@@ -62,6 +73,7 @@ class gatewayConfig:
         self.mqttclient.loop_background()
 
         while True:
+            # Send new message to Adafruit
             self.publishData()
             
             time.sleep(15)
