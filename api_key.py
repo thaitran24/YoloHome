79734879d92edb62935007ada48f46a0ca4bef1f
Kullka <loinguyen07042002@gmail.com>
2023-03-21 22:40:47 +0700

    api_key

---
+++ api_key.py
@@ -0,0 +1,10 @@
+
+"""Feed ID"""
+# AIO_FEED_ID = ['yolohome-full.humidsensor', 'yolohome-full.led1', 'yolohome-full.led2', 'yolohome-full.lightsensor', 'yolohome-full.tempsensor']
+# AIO_FEED_ID2 = ['yolohome-nosensor.fan', 'yolohome-nosensor.servo']
+
+
+
+"""AdaFruit API Key"""
+AIO_USERNAME = 'thaitran24'
+AIO_KEY = 'aio_KXJk83yJIpFEZCP9yRpFvMgVYujT'
