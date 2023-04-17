3060a36cf97d26018430b4429b57e5cd42b4eb14
ptmsk <th.n.h.2.40.31@gmail.com>
2023-04-17 23:30:21 +0700

    rm cache and delete untracked files

--- mobile/env.js
+++
@@ -1,6 +0,0 @@
-// export const baseURL = "http://172.20.10.2:5000";
-export const baseURL = "http://192.168.1.10:5000";
-export const ADA_USER = "thaitran24";
-export const ADA_KEY = "aio_Qidi03MJyprid2DsW1NOoSgi1Rq0";
-// export const ADA_USER = "nhathuytk1";
-// export const ADA_KEY = "aio_GjHW35mjKyP2byXnKzmql0rl0Jkm";
